d7d6116c87d2ccab235359f9d93fd36d97ab30c7
nagendrachahar <nagendras.chahar@gmail.com>
2016-12-10 23:12:34 +0530

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -3,7 +3,7 @@ var slide = document.getElementById('fade');
 setInterval(sowSlide,1000);
 function sowSlide(){
     if(slide.getAttribute("src") == "images/Auto_Monsoon2016_PCBunk._UX1500_SX1500_CB271069653_.jpg"){
-    	slide.src = "/home/manmohan/projects/shivam-chahar/flipkart/images/music-band.jpg";
+    	slide.src = "images/music-band.jpg";
     }else{
     	slide.src = "images/Auto_Monsoon2016_PCBunk._UX1500_SX1500_CB271069653_.jpg"
     }

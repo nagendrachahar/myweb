e147a97eb60ad91f468b9dd855d5b994a19f1c32
nagendrachahar <nagendras.chahar@gmail.com>
2016-12-11 15:21:01 +0530

    Update product-page.js

--- jquery/product-page.js
+++ jquery/product-page.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
             $(window).scroll(function(){
-            	if(document.body.scrollTop > 50){
+            	if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
                 $(".bottom-header").hide(1000);
             }else{
             	$(".bottom-header").show(1000);
